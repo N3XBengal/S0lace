61290a5c588d5b9a4af80b874688f422165de70e
grant <grantschmidt8@gmail.com>
2025-12-09 18:14:07 -0600

    Create firebase.js

---
+++ public/firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDZ9RpsmhIb6wMXCmysScNqfGY4K0Y3GvQ",
+  authDomain: "s0lace.firebaseapp.com",
+  projectId: "s0lace",
+  storageBucket: "s0lace.firebasestorage.app",
+  messagingSenderId: "987787551956",
+  appId: "1:987787551956:web:ffc45e1cc11b0bee34c741",
+  measurementId: "G-W79F0ZGEPN"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+</script>
